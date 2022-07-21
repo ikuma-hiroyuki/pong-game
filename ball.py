43966c5021dd9de62295e077f5ad86264374427d
ikuma-hiroyuki <chemicalbondo@gmail.com>
2022-07-21 18:10:48 +0900

    ballの反射を実装

--- ball.py
+++ ball.py
@@ -1,5 +1,7 @@
 from turtle import Turtle
 
+BALL_SPEED = 0.1
+
 
 class Ball(Turtle):
     def __init__(self):
@@ -7,8 +9,16 @@ class Ball(Turtle):
         self.shape("circle")
         self.penup()
         self.color("white")
+        self.x_move = BALL_SPEED
+        self.y_move = BALL_SPEED
 
     def move(self):
-        new_x = self.xcor() + 1
-        new_y = self.ycor() + 1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(x=new_x, y=new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- main.py
+++ main.py
@@ -28,4 +28,14 @@ while gami_is_on:
     ball.move()
     screen.update()
 
+    # 上下の壁にぶつかったら反射
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # パドルにぶつかったら反射
+    hitting_x = (ball.distance(r_paddle) < 50 and ball.xcor() > 330)
+    hitting_y = (ball.distance(l_paddle) < 50 and ball.xcor() < -330)
+    if hitting_x or hitting_y:
+        ball.bounce_x()
+
 screen.exitonclick()
